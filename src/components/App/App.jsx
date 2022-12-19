0c5298fa84190b8e5ba525250e92e38cea51c934
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-20 00:55:56 +0200

    revrote class on hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,100 +8,166 @@ import { ModalWindow } from '../Modal/Modal';
 import { Button } from '../Button/Button';
 import BeatLoader from 'react-spinners/BeatLoader';
 import toast, { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const override = {
   display: 'block',
   margin: '0 auto',
 };
 
-export class App extends Component {
-  state = {
-    page: 1,
-    querry: '',
-    images: [],
-    url: null,
-    isLoading: false,
-    totalPages: null,
-  };
+export const App = () => {
+  const [page, setPage] = useState(1);
+  const [querry, setQuerry] = useState('');
+  const [images, setImages] = useState([]);
+  const [url, setUrl] = useState(null);
+  const [totalPages, setTotalPages] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async componentDidUpdate(_, prevState) {
-    const { page, querry } = this.state;
-
-    if (prevState.page !== page || prevState.querry !== querry) {
+  useEffect(() => {
+    async function fetchQuerry() {
       try {
-        this.setState({ isLoading: true });
-
         const images = await getImages(querry, page);
-
-        images.hits.length === 0
-          ? toast.error('Зображень не знайдено')
-          : this.setState(prevState => ({
-              images: [...prevState.images, ...images.hits],
-              totalPages: Math.ceil(images.total / 12),
-            }));
+        setImages(prevImages => [...prevImages, ...images.hits]);
       } catch (error) {
-        console.log(error);
         toast.error('Помилка, перезагрузіть сторінку');
-      } finally {
-        this.setState({
-          isLoading: false,
-        });
       }
     }
-  }
-  setQuerryToState = querry => {
+    fetchQuerry();
+  }, [page]);
+
+  const setQuerryToState = querry => {
     if (querry === '') {
       toast.error('Введіть ключове слово');
       return;
     }
-    this.setState({
-      images: [],
-      querry,
-      page: 1,
-    });
+    setImages([]);
+    setQuerry(querry);
+    setPage(1);
   };
 
-  getModalimageUrl = (imageUrl, alt) => {
-    this.setState({
-      url: {
-        imageUrl,
-        alt,
-      },
-    });
+  const getModalimageUrl = (imageUrl, alt) => {
+    setUrl({ imageUrl, alt });
   };
 
-  closeModal = () => {
-    this.setState({ url: null });
+  const closeModal = () => {
+    setUrl(null);
   };
 
-  incrementPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const incrementPage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, url, isLoading, page, totalPages } = this.state;
-    const showButton = page < totalPages;
-    console.log(showButton);
+  const showButton = page < totalPages;
 
-    return (
-      <GalleryApp>
-        <Searchbar onSubmit={this.setQuerryToState} />
-        <BeatLoader
-          color="#3f51b5"
-          loading={isLoading}
-          cssOverride={override}
-          size={30}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-        <ImageGallery images={images} getUrl={this.getModalimageUrl} />
-        {showButton && <Button incrementPage={this.incrementPage} />}
-        {url && <ModalWindow url={url} closeModal={this.closeModal} />}
-        <Toaster position="top-right" />
-        <GlobalStyle />
-      </GalleryApp>
-    );
-  }
-}
+  return (
+    <GalleryApp>
+      <Searchbar onSubmit={setQuerryToState} />
+      <BeatLoader
+        color="#3f51b5"
+        loading={isLoading}
+        cssOverride={override}
+        size={30}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <ImageGallery images={images} getUrl={getModalimageUrl} />
+      {showButton && <Button incrementPage={incrementPage} />}
+      {url && <ModalWindow url={url} closeModal={closeModal} />}
+      <Toaster position="top-right" />
+      <GlobalStyle />
+    </GalleryApp>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     page: 1,
+//     querry: '',
+//     images: [],
+//     url: null,
+//     isLoading: false,
+//     totalPages: null,
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     const { page, querry } = this.state;
+
+//     if (prevState.page !== page || prevState.querry !== querry) {
+//       try {
+//         this.setState({ isLoading: true });
+
+//         const images = await getImages(querry, page);
+
+//         images.hits.length === 0
+//           ? toast.error('Зображень не знайдено')
+//           : this.setState(prevState => ({
+//               images: [...prevState.images, ...images.hits],
+//               totalPages: Math.ceil(images.total / 12),
+//             }));
+//       } catch (error) {
+//         console.log(error);
+//         toast.error('Помилка, перезагрузіть сторінку');
+//       } finally {
+//         this.setState({
+//           isLoading: false,
+//         });
+//       }
+//     }
+//   }
+//   setQuerryToState = querry => {
+//     if (querry === '') {
+//       toast.error('Введіть ключове слово');
+//       return;
+//     }
+//     this.setState({
+//       images: [],
+//       querry,
+//       page: 1,
+//     });
+//   };
+
+//   getModalimageUrl = (imageUrl, alt) => {
+//     this.setState({
+//       url: {
+//         imageUrl,
+//         alt,
+//       },
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ url: null });
+//   };
+
+//   incrementPage = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { images, url, isLoading, page, totalPages } = this.state;
+//     const showButton = page < totalPages;
+//     console.log(showButton);
+
+//     return (
+//       <GalleryApp>
+//         <Searchbar onSubmit={this.setQuerryToState} />
+//         <BeatLoader
+//           color="#3f51b5"
+//           loading={isLoading}
+//           cssOverride={override}
+//           size={30}
+//           aria-label="Loading Spinner"
+//           data-testid="loader"
+//         />
+//         <ImageGallery images={images} getUrl={this.getModalimageUrl} />
+//         {showButton && <Button incrementPage={this.incrementPage} />}
+//         {url && <ModalWindow url={url} closeModal={this.closeModal} />}
+//         <Toaster position="top-right" />
+//         <GlobalStyle />
+//       </GalleryApp>
+//     );
+//   }
+// }
